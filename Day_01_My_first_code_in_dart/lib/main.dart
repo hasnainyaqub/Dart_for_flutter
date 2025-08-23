45ac2030dbe4781745c5f8659617c4489bc5aaab
Hasnain Yaqoob <hasnainyaqubakm@gmail.com>
2025-08-23 13:43:46 +0500

    Learning_Dart

--- Day_01_My_first_code_in_dart/lib/main.dart
+++ Day_01_My_first_code_in_dart/lib/main.dart
@@ -1,5 +1,6 @@
 void main(){
-  print("Hello, It's my first python program in dart language");
-  print("I am learning Data science as my goal, flutter is my interest");
-
+  print("****"*16);
+  print("* Hello, It's my first python program in dart language         *");
+  print("* I am learning Data science as my goal,flutter is my interest *");
+  print("****"*16);
   }
